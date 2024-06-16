42585dead5239aa0becdff4e44d1fe69315a7700
R0rt1z2 <roger@r0rt1z2.com>
2024-06-16 16:23:20 +0200

    libui: Update android::GraphicBufferMapper::lock shims
    * Commit Ib4590b7a3b839a993f6c747c5f09a6312f7ab329 restored
      GraphicBufferMapper::lock old overload, but some libs still require
      the new one.
    
    Change-Id: Ie1435961dc43f7750a80b9986977194835d916f3

--- compat/libui/GraphicBufferMapper.cpp
+++ compat/libui/GraphicBufferMapper.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 The LineageOS Project
+ * Copyright (C) 2024 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -15,11 +15,8 @@ using android::status_t;
 extern "C" {
 status_t _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPvPiS9_(
         void* thisptr, buffer_handle_t handle, uint32_t usage, const Rect& bounds, void** vaddr,
-        int32_t* outBytesPerPixel, int32_t* outBytesPerStride);
-
-status_t _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPv(
-        void* thisptr, buffer_handle_t handle, uint32_t usage, const Rect& bounds, void** vaddr) {
-    return _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPvPiS9_(
-            thisptr, handle, usage, bounds, vaddr, nullptr, nullptr);
+        int32_t* /*outBytesPerPixel*/, int32_t* /*outBytesPerStride*/) {
+    auto* gpm = static_cast<android::GraphicBufferMapper*>(thisptr);
+    return gpm->lock(handle, usage, bounds, vaddr);
 }
 }
